74ae3e9b0d3bedb47918f8d128bc5753a53e706f
Victoria Kee <victoriakee95@gmail.com>
2020-06-11 10:29:41 -0500

    Create a random integer for each var

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 let number1;
 let number2;
+number1 = Math.floor((Math.random() * 10) + 1); //(Math.floor)return largest integer that is <= the given number
+number2 = Math.floor((Math.random() * 10) + 1); //(Random number * 10) & increase number by 1 each time
