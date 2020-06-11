a605c12e555f20a82e86333001b5607a7e73b542
Victoria Kee <victoriakee95@gmail.com>
2020-06-11 10:31:58 -0500

    Declare answer variable

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let number1;
 let number2;
+
 number1 = Math.floor((Math.random() * 10) + 1); //(Math.floor)return largest integer that is <= the given number
 number2 = Math.floor((Math.random() * 10) + 1); //(Random number * 10) & increase number by 1 each time
 document.getElementById('number1').innerHTML = number1; //place the new variables in the HTML
 document.getElementById('number2').innerHTML = number2;
+
+const answer = number1 + numer2;
