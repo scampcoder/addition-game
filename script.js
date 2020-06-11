217309aec61a71dbfb7f59f39e42eeabda11bd4e
Victoria Kee <victoriakee95@gmail.com>
2020-06-11 10:54:13 -0500

    Fix typos and variable issues

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 let number1;
 let number2;
 
-number1 = Math.floor((Math.random() * 10) + 1); //(Math.floor)return largest integer that is <= the given number
-number2 = Math.floor((Math.random() * 10) + 1); //(Random number * 10) & increase number by 1 each time
-document.getElementById('number1').innerHTML = number1; //place the new variables in the HTML
-document.getElementById('number2').innerHTML = number2;
+number1 = Math.floor(Math.random() * 10 + 1); //(Math.floor)return largest integer that is <= the given number
+number2 = Math.floor(Math.random() * 10 + 1); //(Random number * 10) & increase number by 1 each time
+document.getElementById("number1").innerHTML = number1; //place the new variables in the HTML
+document.getElementById("number2").innerHTML = number2;
 
-const answer = number1 + number2;
+let answer = number1 + number2;
 
-const checkAnswer = document.querySelector('input[type=text]');
-const value = checkAnswer.value;
-const button = document.querySelector('input[type=button][value=CHECK]'); //make sure we are selecting the correct button with the value of 'CHECK'
+const checkAnswer = document.querySelector("input[type=text]");
+let value = checkAnswer.value;
+const button = document.querySelector("input[type=button][value=CHECK]"); //make sure we are selecting the correct button with the value of 'CHECK'
 
-btn.onclick = function() {
-  value = checkAnswer.value
-  if (value == answer) { //if user answer is correct
+btn.onclick = function () {
+  value = checkAnswer.value;
+  if (value == answer) {
+    //if user answer is correct
     alert("You are correct!");
   } else {
-    alert ("Bummer! The right answer is " + answer + ".")
+    alert("Bummer! The right answer is " + answer + ".");
   }
-  document.querySelector('input[type=text]').value = '';
-  document.getElementById('number1').innerHTML = '';
-  document.getElementById('number2').innerHTML = '';
-  number1 = Math.floor((Math.random() * 10) + 1);
-  number2 = Math.floor((Math.random() * 10) + 1);
-  document.getElementById('number1').innerHTML = number1;
-  document.getElementById('number2').innerHTML = number2;
+  document.querySelector("input[type=text]").value = "";
+  document.getElementById("number1").innerHTML = "";
+  document.getElementById("number2").innerHTML = "";
+  number1 = Math.floor(Math.random() * 10 + 1);
+  number2 = Math.floor(Math.random() * 10 + 1);
+  document.getElementById("number1").innerHTML = number1;
+  document.getElementById("number2").innerHTML = number2;
 
   answer = number1 + number2;
-
 };
